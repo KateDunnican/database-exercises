08e15899742d884c24f7a86f5540f8168615ecce
Kate McKinney <kate.mckinney210@gmail.com>
2020-04-14 11:25:46 -0500

    part 1 of 4

--- albums_migration.sql
+++ albums_migration.sql
@@ -8,10 +8,10 @@ DROP TABLE IF EXISTS albums;
 
 CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (50) DEFAULT 'Unknown',  -- artist name
-    name VARCHAR (50),                      -- record name
+    artist VARCHAR (100) DEFAULT 'Unknown',  -- artist name
+    name VARCHAR (100),                      -- record name
     release_date INT(4) UNSIGNED NOT NULL,
-    sales FLOAT NOT NULL,
     genre VARCHAR (500) DEFAULT 'None',
+    sales FLOAT NOT NULL,
     PRIMARY KEY (id)
 );
