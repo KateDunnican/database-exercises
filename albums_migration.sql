379de95d3c842a42d130c1848e4969f83d269d4d
Kate McKinney <kate.mckinney210@gmail.com>
2020-04-14 09:31:09 -0500

    finished

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,16 +1,17 @@
 -- codeup_test_user *YoMama123
 -- codeup_test_db
 
+warnings
 USE codeup_test_db;
 
 DROP TABLE IF EXISTS albums;
 
-CREATE TABLE IF NOT EXISTS albums(
+CREATE TABLE albums(
     id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    artist VARCHAR (50),
-    name VARCHAR (50),
-    release_date INT,
-    sales FLOAT,
-    genre VARCHAR (50),
+    artist VARCHAR (50) DEFAULT 'Unknown',  -- artist name
+    name VARCHAR (50),                      -- record name
+    release_date INT(4) UNSIGNED NOT NULL,
+    sales FLOAT NOT NULL,
+    genre VARCHAR (500) DEFAULT 'None',
     PRIMARY KEY (id)
 );
